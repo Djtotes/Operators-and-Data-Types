8bc754270f107b348fd7023b9e70bac7793ee31a
Djtotes <successokorie78@gmail.com>
2025-01-04 16:46:33 +0100

    hmm

---
+++ p1.py
@@ -0,0 +1,7 @@
+a,b,c,d,e,f=34, 34.98, 0.4e-2, 0.3+4j, "90", True
+print("Data type of ",a,"is",type(a))
+print("Data type of ",b,"is",type(b))
+print("Data type of ",c,"is",type(c))
+print("Data type of ",d,"is",type(d))
+print("Data type of ",e,"is",type(e))
+print("Data type of ",f,"is",type(f))
