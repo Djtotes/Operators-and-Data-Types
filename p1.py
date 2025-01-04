4eb6d1a02b4f51c30ce098f414ad1861b3ad63f2
Djtotes <successokorie78@gmail.com>
2025-01-04 17:35:18 +0100

    Lola

--- p1.py
+++ p1.py
@@ -1,7 +1,22 @@
 a,b,c,d,e,f=34, 34.98, 0.4e-2, 0.3+4j, "90", True
 print("Data type of ",a,"is",type(a))
+x=float(a)
+y=str(a)
+z=bool(a)
+print("Data type of ",x,"is",type(a))
+print("Data type of ",y,"is",type(a))
+print("Data type of ",z,"is",type(z))
+
+
 print("Data type of ",b,"is",type(b))
 print("Data type of ",c,"is",type(c))
 print("Data type of ",d,"is",type(d))
 print("Data type of ",e,"is",type(e))
 print("Data type of ",f,"is",type(f))
+
+34 : integer
+34.98 : float
+0.4e-2 : 0.4 x 10 ^-2  : 0.4 x 1/100 : 0.004
+0.3+4j : complex
+"90", 'abc', hello : strings 
+True/False
---
+++ p3.py
@@ -0,0 +1,8 @@
+s = "Amazing "
+print(s[2]) #M
+print(s[-7]) #A
+print(s[2:8]) #M,A,Z,I,N,G
+print(s[1:6:2])
+print(s[-1:-7:-3])
+
+ACP: String opertaions of the one we did today
